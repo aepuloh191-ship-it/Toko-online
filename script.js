987a44079d9bc02273b5d9854b0f0aa5bb13876d
aepuloh191-ship-it <aepuloh191@gmail.com>
2026-01-12 16:52:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function pesan(namaProduk) {
-  const nomorWA = "62852835159480"; // ganti nomor kamu
+  const nomorWA = "6285283515948"; // ganti nomor kamu
   const pesan = `Halo, saya mau pesan ${namaProduk}`;
   const url = `https://wa.me/${nomorWA}?text=${encodeURIComponent(pesan)}`;
   window.open(url, "_blank");
